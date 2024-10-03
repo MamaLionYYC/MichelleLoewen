72fb87fd7a74e33d1e6164a32307012dc1a694a9
Michelle Loewen <mamalionyyc@gmail.com>
2024-10-03 14:45:24 -0600

    commit

---
+++ Assignments/Link-tree/index.js
@@ -0,0 +1,10 @@
+let name = "Bingo";
+
+// // split the name to an array
+let nameArray = name.split("Bingo");
+// // reverse the array
+let reverseArray = nameArray.reverse();
+// // join the array to a string
+let reverseName = reverseArray.join("Bingo");
+
+console.log(reverseName);
